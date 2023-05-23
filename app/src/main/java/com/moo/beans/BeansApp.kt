2cc651498401d8acde9242171bf119f0cbac6729
Jason <jwang11211@gmail.com>
2023-05-23 16:26:15 -0400

    Fixed equations and fleshed out UI

--- app/src/main/java/com/moo/beans/BeansApp.kt
+++ app/src/main/java/com/moo/beans/BeansApp.kt
@@ -5,7 +5,9 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ShapeDefaults
 import androidx.compose.material3.Slider
@@ -17,7 +19,10 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.moo.beans.viewmodel.BeansViewModel
 import com.moo.beans.viewmodel.BeansViewModelFactory
@@ -30,19 +35,25 @@ fun BeansApp() {
         Column(
             modifier = Modifier
                 .fillMaxSize()
-                .padding(16.dp), horizontalAlignment = Alignment.Start
+                .padding(16.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
         ) {
             TextField(
-                modifier = Modifier.padding(8.dp),
+                modifier = Modifier
+                    .padding(8.dp)
+                    .height(120.dp)
+                    .width(280.dp),
+                textStyle = TextStyle(fontSize = 64.sp, textAlign = TextAlign.Center),
                 value = viewModel.total.value,
                 onValueChange = { viewModel.total.value = it },
-                label = { Text(text = "Total") },
+                label = null,
                 shape = ShapeDefaults.ExtraLarge,
                 colors = TextFieldDefaults.textFieldColors(
                     unfocusedIndicatorColor = Color.Transparent,
                     focusedIndicatorColor = Color.Transparent,
                     disabledIndicatorColor = Color.Transparent,
-                )
+                ),
+                singleLine = true,
             )
             Row(
                 modifier = Modifier.fillMaxWidth(),
@@ -52,18 +63,18 @@ fun BeansApp() {
                 Slider(
                     modifier = Modifier
                         .padding(8.dp)
-                        .fillMaxWidth(.90f),
-                    value = viewModel.percent.value,
+                        .fillMaxWidth(.85f),
+                    value = viewModel.getTipPercentage(),
                     onValueChange = { viewModel.percent.value = it },
-                    valueRange = (.10f)..(.20f),
+                    valueRange = (10f)..(20f),
                     steps = 9,
 
                     )
-                Text(text = viewModel.percent.value.toString())
+                Text(text = "${viewModel.getTipPercentage().toInt()}%",
+                fontSize = 20.sp)
             }
-            Text(text = viewModel.getTip())
-            Text(text = viewModel.getTipPlusTotal())
-
+            Text(text = viewModel.getTip(), fontSize = 48.sp)
+            Text(text = viewModel.getTipPlusTotal(), fontSize = 48.sp)
         }
     }
 }
--- app/src/main/java/com/moo/beans/viewmodel/BeansViewModel.kt
+++ app/src/main/java/com/moo/beans/viewmodel/BeansViewModel.kt
@@ -2,13 +2,12 @@ package com.moo.beans.viewmodel
 
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
-import java.math.BigDecimal
 import java.math.RoundingMode
 
 class BeansViewModel: ViewModel() {
     val tip =  mutableStateOf("")
     var total =  mutableStateOf("")
-    val percent =  mutableStateOf(.15f)
+    val percent =  mutableStateOf(15f)
 
 //    fun setTotal(s: String) {
 //        total.value = s.toDouble()
@@ -18,14 +17,14 @@ class BeansViewModel: ViewModel() {
 //        return total.value
 //    }
 
-    fun getTipPercentage(): String {
-
+    fun getTipPercentage(): Float {
+        return percent.value
     }
+
     fun getTip(): String {
         return if (total.value > 0.toString()) {
-            val calc: BigDecimal = (total.value.toBigDecimal() * percent.value.toBigDecimal())
-            calc.setScale(0, RoundingMode.HALF_UP)
-            tip.value = (calc * BigDecimal(100)).toString()
+            val calc = (total.value.toFloat().times(percent.value.div(100))).toBigDecimal()
+            tip.value = calc.setScale(2, RoundingMode.HALF_UP).toString()
             return tip.value
         } else {
             "0"
@@ -33,7 +32,7 @@ class BeansViewModel: ViewModel() {
     }
     fun getTipPlusTotal(): String {
         return if (total.value > 0.toString() && tip.value > 0.toString()) {
-            (total.value.toBigDecimal() + tip.value.toBigDecimal()).toString()
+            (total.value.toBigDecimal() + tip.value.toBigDecimal()).setScale(2, RoundingMode.HALF_UP).toString()
         } else {
             "0"
         }
