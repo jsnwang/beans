5fe0b762c1ca774228b9a989e0e16f58fe8f711b
Jason <jwang11211@gmail.com>
2023-05-24 11:52:05 -0400

    Moved strings to strings.xml

--- app/src/main/java/com/moo/beans/BeansApp.kt
+++ app/src/main/java/com/moo/beans/BeansApp.kt
@@ -15,6 +15,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.moo.beans.ui.TotalTextField
@@ -62,7 +63,9 @@ fun BeansApp(viewModel: BeansViewModel) {
                     fontSize = 20.sp
                 )
             }
+            Text(text = stringResource(id = R.string.tip), fontSize = 30.sp)
             Text(text = viewModel.getTip(), fontSize = 48.sp)
+            Text(text = stringResource(id = R.string.total), fontSize = 30.sp)
             Text(text = viewModel.getTipPlusTotal(), fontSize = 48.sp)
         }
     }
